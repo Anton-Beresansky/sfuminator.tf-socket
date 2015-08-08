60b10e7023405e6d4294ec38ed870d9b1e7a60a1
Aaron <axolo6@gmail.com>
2015-08-08 21:00:55 +0200

    Changed get and added fetch to tf2Currency

--- modules/tf2/tf2Currency.js
+++ modules/tf2/tf2Currency.js
@@ -3,26 +3,29 @@ module.exports = new TF2Currency();
 
 function TF2Currency() {
     this.log = new Logs("TF2 Currency");
+    this._currency = {};
 }
 
 TF2Currency.prototype.setCloud = function (cloud) {
     this.cloud = cloud;
 };
 
+TF2Currency.prototype.get = function () {
+    return this._currency;
+};
+
 TF2Currency.prototype.update = function (callback) {
     var self = this;
     this.log.debug("Updading...");
-    this.get(function (currency) {
-        for (var i in currency) {
-            self[i] = currency[i];
-        }
+    this.fetch(function (currency) {
+        self._currency = currency;
         if (typeof callback === "function") {
-            callback(self);
+            callback(self.get());
         }
     });
 };
 
-TF2Currency.prototype.get = function (callback) {
+TF2Currency.prototype.fetch = function (callback) {
     this.cloud.send("getCurrency", {data: "something..."}, function (currency) {
         if (typeof callback === "function") {
             callback(currency);
