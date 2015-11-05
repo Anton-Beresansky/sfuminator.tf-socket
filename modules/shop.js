3bfe3ad712383e20af12938f8035079b11937631
Aaron <axolo6@gmail.com>
2015-11-05 01:37:19 +0100

    Decreased limit to 4

--- modules/shop.js
+++ modules/shop.js
@@ -31,7 +31,7 @@ function Shop(sfuminator) {
     this.inventory = new ShopInventory(this);
     this.reservations = new Reservations(this.db);
     this.instanceID = new Date().getTime();
-    this.countLimit = {Strange: 0, Vintage: 3, Genuine: 3, Haunted: 3, _any: 5, _price: {over: 6, limit: 3}};
+    this.countLimit = {Strange: 0, Vintage: 3, Genuine: 3, Haunted: 3, _any: 4, _price: {over: 6, limit: 3}};
     /**
      * @type {ShopItemCount}
      */
