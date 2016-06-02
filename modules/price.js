59e9798c0e87d96e68ab6d1817d9755066683d7f
Aaron <axolo6@gmail.com>
2016-06-02 18:16:16 +0200

    No console log plz

--- modules/price.js
+++ modules/price.js
@@ -44,7 +44,6 @@ Price.prototype.toUSD = function () {
 Price.prototype.toMetal = function () {
     if (!this._metalPrice) {
         this._metalPrice = parseInt(this.absolute_price * this.currency.usd.metal * 100 + 0.1) / 100;
-        console.log(this._metalPrice);
     }
     return this._metalPrice;
 };
