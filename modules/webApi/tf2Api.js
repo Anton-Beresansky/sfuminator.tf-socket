5f5bcb3345b1f44df21963ab610b7cb25b46b782
Aaron <axolo6@gmail.com>
2016-09-03 15:51:25 +0200

    Updated schema managing

--- modules/webApi/tf2Api.js
+++ modules/webApi/tf2Api.js
@@ -1,24 +1,28 @@
 module.exports = TF2Api;
+
 var events = require("events");
 var API = require("../../lib/api.js");
 var Logs = require("../../lib/logs.js");
+var request = require("request");
+var cheerio = require("cheerio");
 
 /**
- * @param db
- * @param steam
+ * @param webApi {WebApi}
  * @param backpacktf_key
  * @param options
  * @constructor
  */
-function TF2Api(db, steam, backpacktf_key, options) {
+function TF2Api(webApi, backpacktf_key, options) {
     var self = this;
-    this.db = db;
-    this.log = new Logs({applicationName: "TF2 Api"});
-    this.updateInterval = (options && options.hasOwnProperty("update_interval")) ? options.update_interval : (4 * 60 * 60000); //default 4 hours
-    this.debug = (options && options.hasOwnProperty("debug")) ? options.debug : false; //default false  
-    this.steam = steam;
-    this.bptfApi = new API("backpack.tf");
+    this.webApi = webApi;
+    this.db = webApi.db_items;
+    this.steam = webApi.steamApi;
     this.bptftf_key = backpacktf_key;
+
+    this.updateInterval = (options && options.hasOwnProperty("update_interval")) ? options.update_interval : (4 * 60 * 60000); //default 4 hours
+    this.log = new Logs({applicationName: "TF2 Api"});
+    this.debug = (options && options.hasOwnProperty("debug")) ? options.debug : false; //default false
+    this.bptfApi = new API("backpack.tf");
     events.EventEmitter.call(this);
     this.on("steam_error", function () {
         self.emit("debug", "Steam api returned error, retrying update in 5 minutes");
@@ -172,14 +176,16 @@ TF2Api.prototype.updatePrices = function (callback) {
         if (outdated) {
             self.log.debug("Prices are outdated");
 
+            self.emit("debug", "Getting backpack.tf currencies...");
             self.iGetCurrencies(function (response) {
-                self.emit("debug", "Getting backpack.tf currencies...");
                 if (response.hasOwnProperty("response") && response.response.hasOwnProperty("success") && response.response.success === 1) {
+                    response = self._injectActualKeyPriceToBackpackTFResponse(response);
                     self.saveTF2Currency(response.response.currencies, function () {
-
                         self.iGetPrices(function (response) {
                             self.emit("debug", "Got backpack.tf prices...");
                             if (response.hasOwnProperty("response") && response.response.hasOwnProperty("success") && response.response.success === 1) {
+                                //To avoid price discordance due to actual key price
+                                response.response.items = self._convertBackpackTFPricesToMetal(response.response.items);
                                 self.saveItemPrices(response.response.items, function () {
                                     callback();
                                 });
@@ -216,7 +222,7 @@ TF2Api.prototype.updateSchema = function (callback) {
     var self = this;
     this.isSchemaUpToDate(function (newVersion) {
         self.emit("debug", "New version is: " + newVersion);
-        if (newVersion > 0) {
+        if (newVersion > 0 || true) {
             self.downloadSchema(newVersion, function () {
                 if (typeof callback === "function") {
                     callback();
@@ -314,9 +320,11 @@ TF2Api.prototype.downloadSchema = function (newVersion, callback) {
     this.steam.getSchema(function (schema) {
         self.emit("debug", "Downloaded new schema");
         if (schema.hasOwnProperty("result") && schema.result.hasOwnProperty("status") && schema.result.status === 1) {
-            self.saveSchema(newVersion, schema, function () {
-                self.emit("debug", "Saved new schema");
-                callback(schema);
+            self._injectMarketData(schema, function (modifiedSchema) {
+                self.saveSchema(newVersion, modifiedSchema, function () {
+                    self.emit("debug", "Saved new schema");
+                    callback(modifiedSchema);
+                });
             });
         } else {
             self.emit("steam_error");
@@ -372,6 +380,205 @@ TF2Api.prototype.getLocalSchemaVersion = function (callback) {
             }
         });
     });
+};
+
+TF2Api.prototype._injectMarketData = function (schema, callback) {
+    var self = this;
+    var items = schema.result.items;
+    this._injectMarketNames(items, function (injectedItems1) {
+        self._injectMarketImages(injectedItems1, function (injectedItems2) {
+            schema.result.items = injectedItems2;
+            callback(schema);
+        });
+    });
+};
+
+TF2Api.prototype._injectMarketNames = function (items, callback) {
+    this._fetchDefindexNameLookup(function (defindexLookup) {
+        for (var i = 0; i < items.length; i += 1) {
+            if (items[i].item_quality === 15) {
+                items[i].item_name = defindexLookup[items[i].defindex];
+            }
+        }
+        callback(items);
+    });
+};
+
+TF2Api.prototype._fetchDefindexNameLookup = function (callback) {
+    var self = this;
+    request('https://wiki.alliedmods.net/Team_fortress_2_item_definition_indexes', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            try {
+                var $ = cheerio.load(body);
+                var defindexLookup = {};
+                $("tr").each(function () {
+                    var defindexCell = $(this).find("th").first();
+                    if (defindexCell.attr("scope") === "row") {
+                        defindexLookup[defindexCell.text().trim()] = $(this).find("td").first().text().trim();
+                    }
+                });
+                callback(defindexLookup);
+            } catch (e) {
+                self.log.error("Something went wrong fetching the defindex page");
+                console.log(e);
+                callback(null);
+            }
+        } else {
+            self.log.error("Something went wrong fetching the defindex page (actually their fault)");
+            callback(null);
+        }
+    });
+};
+
+TF2Api.prototype._injectMarketImages = function (items, callback) {
+    var self = this;
+    var i = -1;
+    var nextItem = function () {
+        i += 1;
+        if (i < items.length) {
+            if ((items[i].hasOwnProperty("tool") && items[i].tool.type == "paint_can" && items[i].item_name !== "Paint Can")
+                || items[i].item_quality == 15) {
+                self._getMarketImageInjectedItem(items[i], function (item) {
+                    items[i] = item;
+                    nextItem();
+                });
+            } else {
+                nextItem();
+            }
+        } else {
+            callback(items);
+        }
+    };
+    nextItem();
+};
+
+TF2Api.prototype._getMarketImageInjectedItem = function (item, callback) {
+    var self = this;
+    this._hasItemMarketImage(item.defindex, function (dbUrl, dbUrlLarge) {
+        if (dbUrl) {
+            item.image_url = dbUrl;
+            item.image_url_large = dbUrlLarge;
+            callback(item);
+        } else {
+            self.log.debug("URL for item: " + item.item_name + " not saved, updating...");
+            self._fetchMarketItemImageURL(item, function (url) {
+                item.image_url = url;
+                item.image_url_large = url;
+                callback(item);
+            });
+        }
+    });
+};
+
+TF2Api.prototype._hasItemMarketImage = function (defindex, callback) {
+    this.db.connect(function (connection) {
+        connection.query("SELECT `image_url`,`image_url_large` FROM `schema` WHERE defindex=" + defindex, function (result, isEmpty) {
+            connection.release();
+            if (!isEmpty) {
+                if (result[0].image_url[0] === "[" && result[0].image_url_large[0] === "[") {
+                    callback(result[0].image_url, result[0].image_url_large);
+                } else {
+                    callback(false);
+                }
+            } else {
+                callback(false);
+            }
+        });
+    });
+};
+
+TF2Api.DECORATED_WEARING = [
+    "Factory New",
+    "Minimal Wear",
+    "Field-Tested",
+    "Well-Worn",
+    "Battle Scarred"
+];
+
+TF2Api.prototype._fetchMarketItemImageURL = function (schemaItem, callback) {
+    var self = this;
+    var names = [], images = [];
+
+    if (schemaItem.hasOwnProperty("tool") && schemaItem.tool.type == "paint_can" && schemaItem.item_name !== "Paint Can") {
+        names.push(schemaItem.item_name);
+    } else if (schemaItem.item_quality == 15) {
+        for (var i = 0; i < TF2Api.DECORATED_WEARING.length; i += 1) {
+            names.push(schemaItem.item_name + " " + schemaItem.item_type_name + " (" + TF2Api.DECORATED_WEARING[i] + ")");
+        }
+    }
+
+    var fetchImages = function (names) {
+        var i = -1;
+        var fetch = function (name, callback) {
+            request('http://steamcommunity.com/market/listings/440/' + encodeURIComponent(name.trim()), function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    try {
+                        var $ = cheerio.load(body);
+                        callback($(".market_listing_largeimage > img").attr("src").slice(0, -10));
+                    } catch (e) {
+                        self.log.error("Something went wrong fetching the item page (" + name + ")");
+                        console.log(e);
+                        callback("");
+                    }
+                } else {
+                    self.log.error("Something went wrong fetching the item page (actually steam fault) (" + name + ")");
+                    callback("");
+                }
+            });
+        };
+        var next = function () {
+            setTimeout(function () {
+                i += 1;
+                if (i < names.length) {
+                    fetch(names[i], function (url) {
+                        images.push(url);
+                        next();
+                    });
+                } else {
+                    callback(JSON.stringify(images))
+                }
+            }, 2500);
+        };
+        next();
+    };
+    fetchImages(names);
+};
+
+TF2Api.prototype._injectActualKeyPriceToBackpackTFResponse = function (response) {
+    this.backpackTFKeyPrice = response.response.currencies.keys.price.value;
+    response.response.currencies.keys.price.value = this.webApi.keyPricer.getMarketPrice().toMetal();  //Inject actual key price
+    return response;
+};
+
+TF2Api.prototype._convertBackpackTFPricesToMetal = function (items) {
+    //Remember to apply our key price first
+    items["Mann Co. Supply Crate Key"].prices["6"].Tradable.Craftable[0].value = this.webApi.keyPricer.getMarketPrice().toMetal();
+    //Go on
+    for (var i in items) {
+        var prices = items[i].prices;
+        for (var quality in prices) {
+            for (var tradable in prices[quality]) {
+                for (var craftable in prices[quality][tradable]) {
+                    if (typeof prices[quality][tradable][craftable][0] === "object") { // This is unusual object OMG NOOB
+                        var this_price = prices[quality][tradable][craftable][0];
+                        if (this_price.currency === "keys") {
+                            prices[quality][tradable][craftable][0].currency = "metal";
+                            prices[quality][tradable][craftable][0].value = prices[quality][tradable][craftable][0].value * this.backpackTFKeyPrice;
+                        }
+                    } else {
+                        for (var unusualParticle in prices[quality][tradable][craftable]) {
+                            var this_price = prices[quality][tradable][craftable][unusualParticle];
+                            if (this_price.currency === "keys") {
+                                prices[quality][tradable][craftable][unusualParticle].currency = "metal";
+                                prices[quality][tradable][craftable][unusualParticle].value = prices[quality][tradable][craftable][unusualParticle].value * this.backpackTFKeyPrice;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return items;
 };
 
 TF2Api.prototype._convertItemPricesFormat = function (items) {
@@ -500,7 +707,7 @@ TF2Api.prototype._convertCurrencyFormat = function (result) {
         },
         hat: {
             usd: hat_usd_price,
-            metal: hat_usd_price/ metal_usd_price,
+            metal: hat_usd_price / metal_usd_price,
             hat: 1,
             keys: hat_usd_price / key_usd_price,
             earbuds: hat_usd_price / earbuds_usd_price
